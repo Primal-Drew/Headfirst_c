f2c0b20d24b672a24732c94bce3b090023ee9c0e
primal-drew <oginaandrew05@gmail.com>
2024-02-02 15:36:23 +0300

    Chapter2: The array an array - 2D arrays

---
+++ Chapter_2-Memory_and_pointers/tracklist.c
@@ -0,0 +1,14 @@
+// This is to track the name of songs in a tracklist.
+#include <stdio.h>
+
+int main(){
+    char track[][80] ={
+        "I left my heart in Harvad Med school",
+        "Newmark,Newmark-A wonderful Town",
+        "Dancing with a Dork",
+        "From here to maternity",
+        "The girl for Iwo Jima"
+    };
+
+    printf("Track 5 is:%s\n",track[4]);
+}

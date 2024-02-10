b7649683bebeca4db637291331fc02f6f441b468
primal-drew <oginaandrew05@gmail.com>
2024-02-10 17:24:43 +0300

    Chapter4: Introduced user prompts

--- Chapter4_using_multiple_source_files/message_hider.c
+++ Chapter4_using_multiple_source_files/message_hider.c
@@ -3,9 +3,11 @@
 int main()
 {
     char msg[80];
+    printf("Enter Message: ");
     while(fgets(msg,80,stdin)){
         encrypt(msg);
         puts(msg);
+        printf("Enter Message: ");
     }
     return 0;
 }

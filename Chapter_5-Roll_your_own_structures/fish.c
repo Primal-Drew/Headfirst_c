b0505d06cd4a2a8569dcedd95cf3464fb38f1b6d
primal-drew <oginaandrew05@gmail.com>
2024-02-12 11:53:50 +0300

    Chapter5: A function that stores diff data types

---
+++ Chapter_5-Roll_your_own_structures/fish.c
@@ -0,0 +1,21 @@
+/* Print out the catalog entry */
+#include <stdio.h>
+
+void catalog(const char* name,const char* species,int teeth, int age){
+    printf("%s is a %s with %i teeth. He is %i\n",name, species, teeth, age);
+}
+
+/* Print the label for the tank */
+
+void label(const char *name,const char *species, int teeth,int age){
+    printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n",name, species, teeth, age);
+}
+
+int main()
+{
+
+    catalog("Snappy", "Piranha", 69, 4);
+    label("Snappy", "Piranha", 69, 4);
+    
+
+}

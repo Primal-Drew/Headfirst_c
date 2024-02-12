82be568051d1483daf5223e4f114618aaacd818b
primal-drew <oginaandrew05@gmail.com>
2024-02-12 13:25:12 +0300

    Chapter5: Updated the functions to the structures instead

--- Chapter_5-Roll_your_own_structures/fish.c
+++ Chapter_5-Roll_your_own_structures/fish.c
@@ -1,21 +1,38 @@
 /* Print out the catalog entry */
+struct fish{
+    const char* name;
+    const char* species;
+    int teeth;
+    int age;
+};
+
+
 #include <stdio.h>
 
-void catalog(const char* name,const char* species,int teeth, int age){
-    printf("%s is a %s with %i teeth. He is %i\n",name, species, teeth, age);
+void catalog(struct fish f){
+    printf("%s is a %s with %i teeth. He is %i\n",f.name, f.species, f.teeth, f.age);
 }
 
 /* Print the label for the tank */
 
-void label(const char *name,const char *species, int teeth,int age){
-    printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n",name, species, teeth, age);
+void label(struct fish f){
+    printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n",f.name, f.species, f.teeth, f.age);
 }
+
+/*Using a structured data type*/
+
+
+
+
 
 int main()
 {
 
-    catalog("Snappy", "Piranha", 69, 4);
-    label("Snappy", "Piranha", 69, 4);
+    struct fish snappy ={"Snappy","Piranha",69,4};
+
+    catalog(snappy);
+    label(snappy);
     
+    return 0; 
 
 }

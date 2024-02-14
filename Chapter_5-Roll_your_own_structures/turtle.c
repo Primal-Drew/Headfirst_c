8699915c54e5af7f063380c7f849c46f9550b676
primal-drew <oginaandrew05@gmail.com>
2024-02-14 10:08:03 +0300

    Chapter 5: Updating values in structs

--- Chapter_5-Roll_your_own_structures/turtle.c
+++ Chapter_5-Roll_your_own_structures/turtle.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 typedef struct
 {
     const char *name;
@@ -7,17 +8,43 @@ typedef struct
     int age;
 } turtle;
 
-void happy_birthday(turtle t)
+/*
+The function below attempts to  update the value of age in a struct.
+    We use a pointer to the value because of the following reasons:
+
+        1. Directly function(turtle .t) would simply clone the values
+            the structure...But we want to update the real struct
+
+        2. Using a pointer we are able to get the address of the real
+            struct value we want to modify instead of cloning it.
+*/
+
+void happy_birthday(turtle *t)
 {
-    t.age = t.age + 1;
+    /*
+    Its crucial to use (*t).age instead of *t.age...
+
+        (*t).age or t->age 
+            This means the age of the turtle pointed to by t
+
+        *t.age or *(t.age)
+            This means the value at the memory location given by (t.age)
+                while there isn't a memory location called t.age
+
+    */
+
+    (*t).age = (*t).age + 1;
     printf("Happy Birthday %s! You are now %i years old!\n",
-           t.name, t.age);
+           (*t).name, (*t).age);
 }
 
 int main()
 {
     turtle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
-    happy_birthday(myrtle);
+
+    happy_birthday(&myrtle);
+
     printf("%s's age is now %i\n", myrtle.name, myrtle.age);
+    
     return 0;
 }

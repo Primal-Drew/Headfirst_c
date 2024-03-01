e8546a9ebcadbd0515bfb4858b4243a8eec1aa1e
primal-drew <oginaandrew05@gmail.com>
2024-03-01 17:54:21 +0300

    Chapter 7: Using strstr() to filter.

--- Chapter_7-Automatic_functions/find.c
+++ Chapter_7-Automatic_functions/find.c
@@ -11,20 +11,38 @@ char *ADS[] = {"  William: SBM GSOH likes sports, TV, dining",
                "Josh: SJM likes sports, movies and theater",
                "Jed: DBM likes theater, books and dining"};
 
-void find(){
+void find(int (*match)(char*)){
     int i;
     puts("Search Results:");
     puts("-----------------------------------------------");
     for (i=0;i<NUM_ADS;i++){
-        if(strstr(ADS[i],"sports") && ! strstr(ADS[i],"bieber"))
+        if(match(ADS[i]))
             printf("%s\n",ADS[i]);
     }
     puts("-----------------------------------------------");
 }
 
+int sports_no_bieber(char *s){
+    return strstr(s,"sports") && ! strstr(s,"bieber");
+}
+
+int sports_or_workout(char *s){
+    return strstr(s,"sports") || strstr(s,"working out"); 
+}
+
+int ns_theater(char *s){
+    return strstr(s,"theater") &&  strstr(s,"NS");
+}
+
+int arts_theater_or_dining(char *s){
+    return strstr(s,"art") || strstr(s,"theater") || strstr(s,"dining");
+}
 
 int main()
 {
-    find();
+    find(sports_no_bieber);
+    find(sports_or_workout);
+    find(ns_theater);
+    find(arts_theater_or_dining);
     return 0;
 }
